e79321db45a7f991dd211ccea6ed9a1d9d9a8153
Aritra1704 <aritra1704@gmail.com>
2021-12-23 13:33:48 +0530

    Created Redux state

--- ppmtoolexpo/App.tsx
+++ ppmtoolexpo/App.tsx
@@ -5,20 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AddProject from './src/components/project/AddProject';
-
+import { Provider } from 'react-redux';
+import store from "./src/store";
 
 const App = () => {
   return (
-    <Router>
-      <div style={styles}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/addProject" element={<AddProject />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div style={styles}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
